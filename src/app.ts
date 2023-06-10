9c6285ddff9bf018ca07cde50608db82f3ac86e5
nazmu957 <nazmus957@gmail.com>
2023-06-10 10:15:52 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-             app.use(cors())
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Working successfully')
 })
 
